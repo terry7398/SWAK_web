9180de92819263297e066e0c132f535d16a3cfa2
Jisung Song <128297715+terry7398@users.noreply.github.com>
2024-07-10 22:23:50 +0900

    `1

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ import uvicorn
 
 app = FastAPI()
 
+client_info = []
+
 app.mount("/public/static", StaticFiles(directory="public/static"), name="static")
 
 templates = Jinja2Templates(directory="public/templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    header_value = request.headers.get("header-name")
-    #return templates.TemplateResponse("index.html", {"request": request}) , 
-    return f"{request.headers}"
-
-@app.get("/reservation/{number}")
-def send(number: int):
-    print(number)
-    return f"{number}번 전송 완료"
+@app.get("/{number}", response_class=HTMLResponse)
+async def read_root(name : str ,request: Request):
+    if ({name : request.headers.get('X-Forwarded-For')} in client_info ):
+        pass
+    else:
+        client_info.append({name : request.headers.get('X-Forwarded-For')})
+    return templates.TemplateResponse("game.html", {"request": request})
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app',
